43cf9dc5bac03b9264f79c931b2b542071d43983
csimi <csimika@gmail.com>
2017-11-30 17:01:48 +0100

    create babel transpiled es modules in dist

--- rollup.config.js
+++ rollup.config.js
@@ -1,27 +1,37 @@
 import babel from 'rollup-plugin-babel';
 
-export default {
-	'input': 'index.js',
-	'output': {
+function getConfig (output) {
+	return {
+		'input': 'index.js',
+		'output': output,
+		'external': [
+			'twig',
+			'jquery',
+		],
+		'plugins': [
+			babel({
+				'exclude': 'node_modules/**',
+				'babelrc': false,
+				'presets': [
+					[
+						'env',
+						{
+							'modules': false,
+						},
+					],
+				],
+			}),
+		],
+	};
+}
+
+export default [
+	getConfig({
 		'file': 'dist/index.js',
 		'format': 'cjs',
-	},
-	'external': [
-		'twig',
-		'jquery',
-	],
-	'plugins': [
-		babel({
-			'exclude': 'node_modules/**',
-			'babelrc': false,
-			'presets': [
-				[
-					'env',
-					{
-						'modules': false,
-					},
-				],
-			],
-		}),
-	],
-};
+	}),
+	getConfig({
+		'file': 'dist/es.js',
+		'format': 'es',
+	}),
+];
